b0d0c5703303ad35bb795d44035e084b719939a9
LizzyOrji123 <cegscutie4rill@yahoo.com>
2023-06-19 15:08:52 +0100

    success in sending mail to mailcatcher

---
+++ app/controllers/applies_controller.rb
@@ -0,0 +1,33 @@
+class AppliesController < ApplicationController
+  def index
+    # @applies = Apply.all
+  end
+
+  def new
+    @apply = Apply.new
+  end
+
+  def show
+    @applies = Apply.find(params[:id])
+    render :show
+  end
+
+  def create
+    @apply = Apply.new(apply_params)
+    if @apply.save
+      # @apply.request = request
+      # @apply.deliver
+      flash.now[:success] = 'Message sent!'
+    else
+      flash.now[:error] = 'Could not send message'
+      render :new
+    end
+    redirect_to apply_path(@apply)
+  end
+end
+
+private
+
+def apply_params
+  params.require(:apply).permit(:name, :dob, :gender, :marriage, :occupation, :email, :phone, :country, :photo)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
   get "why", to: "pages#why"
   get "how_we_work", to: "pages#how_we_work"
   get "location", to: "pages#location"
-  get "apply", to: "apply#index"
-  post "apply", to: "apply#index"
-  post "apply_create", to: "apply#create"
-  get "apply/new", to: "apply#new"
-  post "apply/new", to: "apply#new"
+  # get "apply", to: "apply#index"
+  # post "apply", to: "apply#index"
+  # post "apply_create", to: "apply#create"
+  # get "apply/new", to: "apply#new"
+  # post "apply/new", to: "apply#new"
+
+  resources :applies
   # get "apply_new", to: "apply#create"
-  get "apply/:id", to: "apply#show", as: :applies
+  # get "apply/:id", to: "apply#show", as: :applies
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
